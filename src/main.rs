f3314b144ccd6cd970328cbed918fff2f287e7b2
mochineko <mochinekoo0113@gmail.com>
2026-02-12 22:01:08 +0900

    [add] 変数を追加しました

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
+    let a = 26; //型推論変数
+    // a = 25; （※これはできない）
+    let mut _aA = 5; //変更可能
+    _aA = 10;
+
+    //型明示
+    let b:i32 = 10; //32ビット整数
+    let c:f64 = 3.14; //64ビット小数点
+    let d: &str = "aa"; //文字列
+
     println!("Hello, world!");
+    println!("a = {}", a);
+    println!("b = {}", b);
 }
