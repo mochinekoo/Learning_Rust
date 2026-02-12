ca52e3298a1b797f8de189b1ff6d818b70ce43f6
mochineko <mochinekoo0113@gmail.com>
2026-02-12 22:25:05 +0900

    [add] win32APIのメッセージボックスを追加しました

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::ptr::null_mut;
+use windows::core::PCWSTR;
+use windows::Win32::UI::WindowsAndMessaging::*;
+
 fn main() {
     let a = 26; //型推論変数
     // a = 25; （※これはできない）
@@ -12,4 +16,19 @@ fn main() {
     println!("Hello, world!");
     println!("a = {}", a);
     println!("b = {}", b);
+
+    let text = "メッセージ";
+    let title = "タイトル";
+
+    let text_w: Vec<u16> = text.encode_utf16().chain(std::iter::once(0)).collect();
+    let title_w: Vec<u16> = title.encode_utf16().chain(std::iter::once(0)).collect();
+
+    unsafe {
+        MessageBoxW(
+            None,
+            PCWSTR(text_w.as_ptr()),
+            PCWSTR(title_w.as_ptr()),
+            MB_OK | MB_HELP,
+        );
+    }
 }
