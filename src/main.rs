d1c26361fa61dd8301ef0a5b3fc7de785de0df11
mochineko <mochinekoo0113@gmail.com>
2026-02-12 22:42:32 +0900

    [add] vectorを追加しました

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,12 @@ fn main() {
     let text_w: Vec<u16> = text.encode_utf16().chain(std::iter::once(0)).collect();
     let title_w: Vec<u16> = title.encode_utf16().chain(std::iter::once(0)).collect();
 
+    let mut list: Vec<&str> = Vec::new();
+    list.push("aaa");
+    list.push("bbb");
+    println!("{:?}", list);
+    println!("{:?}", list[0]);
+
     unsafe {
         MessageBoxW(
             None,
